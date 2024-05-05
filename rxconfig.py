f03f113b111bc67830cbf5a6c188582be21b8b9c
manu-180 <manumanu97@hotmail.com>
2024-05-05 01:19:43 -0300

    backend_deploy

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://api.baackend.com/"
+    api_url="https://api.baackend.com/",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "http://baackend.com"
+    ]
+    
+    
 )

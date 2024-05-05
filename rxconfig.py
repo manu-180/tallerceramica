649a5608873d221d1c4c671af1d1781b8eed1c73
manu-180 <manumanu97@hotmail.com>
2024-05-05 02:18:07 -0300

    .

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    api_url="api.baackend.com"
-
+    api_url="https://api.baackend.com",  # Asegúrate de que esta URL sea correcta
 )

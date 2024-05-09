b066ae9fe6054393e30ad367487289b4b4bb90e6
manu-180 <manumanu97@hotmail.com>
2024-05-08 22:07:32 -0300

    cambio

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://pagina-web-production-4d43.up.railway.app/",  # Asegúrate de que esta URL sea correcta
+    api_url="https://api.baackend.com/"
 )
